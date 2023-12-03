c0bcad1a4fbd1f7e8a4de8aa0d37d4dff15f4184
Eddy Cos <cos_eddy_man@yahoo.com>
2023-12-03 23:53:10 +0200

    more pythonic strucure

--- run_tag_editor.py
+++ run_tag_editor.py
@@ -2,18 +2,16 @@ import os
 import time
 from mp3_tagger import MP3File
 
-# array containing keywords to be removed from the filename
+# List containing keywords to be removed from the filename
 keywords_to_remove = [' myfreemp3.vip', 'X2Download.app - ', 'X2Download.app ', ' (320 kbps)', 'SaveTube.io -']
-words_removal_arr = []
 
-# path to the folder containing the music files
+# Path to the folder containing the music files
 path_to_music = r'D:\Music\test_folder'
 
 
 def read_keyboard_input():
     keywords_nr = int(input('How many keywords do you want to remove?: '))
-    for i in range(keywords_nr):
-        words_removal_arr.append(str(input('Keyword ' + str(i + 1) + ':')))
+    words_removal_arr = [input(f'Keyword {i + 1}:') for i in range(keywords_nr)]
     print(words_removal_arr)
 
 
@@ -25,18 +23,17 @@ def remove_duplicates():
         path_to_file = os.path.join(path_to_music, file)
         if os.path.isfile(path_to_file):
             total_nr_files += 1
-            for k in check_arr:
-                if k in file:
-                    try:
-                        os.remove(path_to_file)
-                        count_duplicates += 1
-                        print('Removed file: ' + file)
-                    except OSError:
-                        print("Error while deleting file ", path_to_file)
+            if any(check in file for check in check_arr):
+                try:
+                    os.remove(path_to_file)
+                    count_duplicates += 1
+                    print(f'Removed file: {file}')
+                except OSError:
+                    print("Error while deleting file ", path_to_file)
     print('--------------------------------------------')
-    print('Duplicates removed: ' + str(count_duplicates))
-    print('Total number of songs: ' + str(total_nr_files))
-    print('Total songs after removal of duplicates: ' + str(total_nr_files - count_duplicates))
+    print(f'Duplicates removed: {count_duplicates}')
+    print(f'Total number of songs: {total_nr_files}')
+    print(f'Total songs after removal of duplicates: {total_nr_files - count_duplicates}')
 
 
 def process_files():
@@ -45,18 +42,12 @@ def process_files():
         path_to_file = os.path.join(path_to_music, file)
         if os.path.isfile(path_to_file):
             output_file_name = file
-            edit_flag = False
             for key in keywords_to_remove:
-                if key in output_file_name:
-                    edit_flag = True
-                    output_file_name = output_file_name.replace(key, "")
-            if edit_flag is True:
-                total_files_edited += 1
-                new_renamed_file_path = os.path.join(path_to_music, output_file_name)
-                os.rename(path_to_file, new_renamed_file_path)
-            else:
-                pass
-    print("Total files processed: " + str(total_files_edited))
+                output_file_name = output_file_name.replace(key, "")
+            new_renamed_file_path = os.path.join(path_to_music, output_file_name)
+            os.rename(path_to_file, new_renamed_file_path)
+            total_files_edited += 1
+    print(f'Total files processed: {total_files_edited}')
 
 
 def show_all_files():
@@ -67,39 +58,35 @@ def show_all_files():
 
 
 def clear_fields(mp3_file):
-    emtpy_field = ''
-    mp3_file.album = emtpy_field
-    mp3_file.band = emtpy_field
-    mp3_file.comment = emtpy_field
-    mp3_file.composer = emtpy_field
-    mp3_file.publisher = emtpy_field
-    mp3_file.url = emtpy_field
-    #mp3_file.genre = emtpy_field
-    mp3_file.year = emtpy_field
+    empty_field = ''
+    mp3_file.album = empty_field
+    mp3_file.band = empty_field
+    mp3_file.comment = empty_field
+    mp3_file.composer = empty_field
+    mp3_file.publisher = empty_field
+    mp3_file.url = empty_field
+    # mp3_file.genre = empty_field
+    mp3_file.year = empty_field
     mp3_file.save()
 
 
 def edit_ID3_tag():
     print('running tag edit...')
-    parser_delimiter_1 = ' - '  # delimiters seen: ' - ' ' – '
-    parser_delimiter_2 = ' – '
+    parser_delimiters = [' - ', ' – ']
     for file in os.listdir(path_to_music):
         path_to_file = os.path.join(path_to_music, file)
         if os.path.isfile(path_to_file) and '.mp3' in file:
-
-            # Create MP3File instance
             mp3 = MP3File(path_to_file)
-
-            # Clear all id3 tag fields
             clear_fields(mp3)
 
-            # under work
-            if parser_delimiter_1 in file:
-                split_song_arr = file.split(parser_delimiter_1)
-                mp3.artist = split_song_arr[0]
-                mp3.song = split_song_arr[1][:-4]
-            elif parser_delimiter_2 in file:
-                split_song_arr = file.split(parser_delimiter_2)
+            found_delimiter = None
+            for delimiter in parser_delimiters:
+                if delimiter in file:
+                    found_delimiter = delimiter
+                    break
+
+            if found_delimiter:
+                split_song_arr = file.split(found_delimiter)
                 mp3.artist = split_song_arr[0]
                 mp3.song = split_song_arr[1][:-4]
             else:
@@ -108,7 +95,7 @@ def edit_ID3_tag():
             mp3.save()
 
 
-def __main__():
+def main():
     # read_keyboard_input()
     start = time.time()
     # remove_duplicates()
@@ -116,7 +103,8 @@ def __main__():
     # show_all_files()
     edit_ID3_tag()
     end = time.time()
-    print('Completed successfully in: ' + "{:.2f}".format(end - start) + ' s')
+    print(f'Completed successfully in: {"{:.2f}".format(end - start)} s')
 
 
-__main__()
+if __name__ == "__main__":
+    main()
